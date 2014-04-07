be56d5c966e33773f9362ad6d9c6ce5f650dcf65
Sam Pfeiffer <sammypfeiffer@gmail.com>
2014-04-08 00:55:48 +0200

    Typo in the folder name

--- apply_xenomai_patches.sh
+++ apply_xenomai_patches.sh
@@ -5,10 +5,10 @@ cd xenorasp
 
 # Apply the patches
 # Apply pre-patch to the arm patch... this is not very clean I've read, but works!
-cd linux-rpi-3.10.y; patch -Np1 ../patch/ipipe-core-3.10.18-arm-1.patch ../patch/rpi-ipipe-core-3.10.34-arm-1.patch
+cd linux-rpi-3.10.y; patch -Np1 ../patches/ipipe-core-3.10.18-arm-1.patch ../patches/rpi-ipipe-core-3.10.34-arm-1.patch
 
 # Apply xenomai ipipe core patch
-xenomai-head/scripts/prepare-kernel.sh --arch=arm --linux=linux-rpi-3.10.y --adeos=patch/ipipe-core-3.10.18-arm-1.patch
+xenomai-head/scripts/prepare-kernel.sh --arch=arm --linux=linux-rpi-3.10.y --adeos=patches/ipipe-core-3.10.18-arm-1.patch
 
 # Apply post-patch
-cd linux-rpi-3.10.y; patch -Np1 < ../patch/ipipe-core-3.10.34-arm-raspberry-post-1.patch
+cd linux-rpi-3.10.y; patch -Np1 < ../patches/ipipe-core-3.10.34-arm-raspberry-post-1.patch
