9785430027d0712fbb1d4625135545bbd9b6c543
Sam Pfeiffer <sammypfeiffer@gmail.com>
2014-04-08 00:47:10 +0200

    Added comments about file sizes

--- download_xenorasp_stuff.sh
+++ download_xenorasp_stuff.sh
@@ -4,18 +4,18 @@
 mkdir xenorasp
 cd xenorasp
 
-# Get the crosscompiler
+# Get the crosscompiler (270MB~)
 wget https://github.com/raspberrypi/tools/archive/master.tar.gz
 tar xzf master.tar.gz
 
-# Get Raspberry Pi Linux kernel
+# Get Raspberry Pi Linux kernel (500MB~)
 git clone -b rpi-3.10.y --depth 1 git://github.com/raspberrypi/linux.git linux-rpi-3.10.y
 
-# Get latest Xenomai
+# Get latest Xenomai (50MB~)
 git clone git://git.xenomai.org/xenomai-head.git xenomai-head
 
 # From this discussion: http://comments.gmane.org/gmane.linux.real-time.xenomai.users/18934
-# Got 3.10.34 patches... copy from my repo
+# Got 3.10.34 patches... copy from this repo
 cp -r ../patches .
 
 # Get official Xenomai I-pipe patches (What is this I-pipe thing? http://www.xenomai.org/index.php/FAQs#What_is_this_I-pipe_thing.3F_And_what_about_Adeos.3F_Are_they_related.3F)
