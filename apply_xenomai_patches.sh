5ec641e60a6d087a9f12929d57c98e636ed1497f
Sam Pfeiffer <sammypfeiffer@gmail.com>
2014-04-08 00:58:26 +0200

    Re-fix paths

--- apply_xenomai_patches.sh
+++ apply_xenomai_patches.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # Being at xenorasp dir...
-cd xenorasp
+cd xenorasp/patches
 
 # Apply the patches
 # Apply pre-patch to the arm patch... this is not very clean I've read, but works!
-cd linux-rpi-3.10.y; patch -Np1 ../patches/ipipe-core-3.10.18-arm-1.patch ../patches/rpi-ipipe-core-3.10.34-arm-1.patch
+patch -Np1 ipipe-core-3.10.18-arm-1.patch rpi-ipipe-core-3.10.34-arm-1.patch
 
+cd ..
 # Apply xenomai ipipe core patch
 xenomai-head/scripts/prepare-kernel.sh --arch=arm --linux=linux-rpi-3.10.y --adeos=patches/ipipe-core-3.10.18-arm-1.patch
 
