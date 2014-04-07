cc824f099c284645cd0da90260d4c7905451f02b
Sam Pfeiffer <sammypfeiffer@gmail.com>
2014-04-08 00:16:21 +0200

    Starting the work to get xenomai 2.6.3 with linux kernel 3.10

--- apply_xenomai_patches.sh
+++ apply_xenomai_patches.sh
@@ -4,11 +4,11 @@
 cd xenorasp
 
 # Apply the patches
-# Apply pre-patch
-cd linux-rpi-3.8.y; patch -Np1 < ../patch/ipipe-core-3.8.13-raspberry-pre-2.patch
+# Apply pre-patch to the arm patch... this is not very clean I've read, but works!
+cd linux-rpi-3.10.y; patch -Np1 ../patch/ipipe-core-3.10.18-arm-1.patch ../patch/rpi-ipipe-core-3.10.34-arm-1.patch
 
 # Apply xenomai ipipe core patch
-xenomai-head/scripts/prepare-kernel.sh --arch=arm --linux=linux-rpi-3.8.y --adeos=xenomai-head/ksrc/arch/arm/patches/ipipe-core-3.8.13-arm-3.patch
+xenomai-head/scripts/prepare-kernel.sh --arch=arm --linux=linux-rpi-3.10.y --adeos=../patch/ipipe-core-3.10.18-arm-1.patch
 
 # Apply post-patch
-cd linux-rpi-3.8.y; patch -Np1 < ../patch/ipipe-core-3.8.13-raspberry-post-2.patch
+cd linux-rpi-3.10.y; patch -Np1 < ../patch/ipipe-core-3.10.34-arm-raspberry-post-1.patch
